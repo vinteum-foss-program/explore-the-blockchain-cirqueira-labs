42def11a264f5344002c216b306f401bcf423f98
cirqueira-labs <jccirqueira@outlook.com>
2024-11-26 07:16:25 -0300

    fix 006.sh

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,25 @@
-# Which tx in block 257,343 spends the coinbase output of block 256,128?
+#!/bin/bash
+
+source_block=256128
+target_block=257343
+
+source_block_hash=$(bitcoin-cli getblockhash $source_block)
+
+target_block_hash=$(bitcoin-cli getblockhash $target_block)
+
+source_coinbase_txid=$(bitcoin-cli getblock $source_block_hash | jq -r '.tx[0]')
+
+source_coinbase_vout=$(bitcoin-cli getrawtransaction $source_coinbase_txid true | jq -r '.vout[0].value')
+
+target_txs=$(bitcoin-cli getblock $target_block_hash | jq -r '.tx[]')
+
+for txid in $target_txs; do
+    inputs=$(bitcoin-cli getrawtransaction $txid true | jq -r '.vin[]?.txid')
+
+    if [[ "$inputs" == *"$source_coinbase_txid"* ]]; then
+        echo "$txid"
+        exit 0
+    fi
+done
+
+
