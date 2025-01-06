f9705ef23050686c42f3d564d14e5f33de7135b8
cirqueira-labs <jccirqueira@outlook.com>
2025-01-06 14:40:09 -0300

    fix:005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -1,26 +1,2 @@
-#!/bin/bash
-
-TXID="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
-
-TX_DETAILS=$(bitcoin-cli getrawtransaction $TXID 1)
-
-PUBKEYS=()
-for input in $(echo $TX_DETAILS | jq -r '.vin[].scriptSig.hex'); do
-    PUBKEY=$(echo $input | cut -d ' ' -f 2)  # Ajuste isso para extrair a chave pública corretamente
-    PUBKEYS+=($PUBKEY)
-done
-
-if [ ${#PUBKEYS[@]} -ne 4 ]; then
-    echo "Erro: Número incorreto de chaves públicas encontradas. Esperado 4, encontrado ${#PUBKEYS[@]}."
-    exit 1
-fi
-
-echo "Chaves públicas extraídas:"
-for pubkey in "${PUBKEYS[@]}"; do
-    echo $pubkey
-done
-
-MULTISIG_COMMAND="bitcoin-cli createmultisig 1 '[${PUBKEYS[0]},${PUBKEYS[1]},${PUBKEYS[2]},${PUBKEYS[3]}]'"
-
-echo $MULTISIG_COMMAND
-
+pub_keys=$(bitcoin-cli getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517 1 | jq [.vin[].txinwitness[1]])
+bitcoin-cli createmultisig 1 "$pub_keys" | jq -r .address
