048682ddbe092ac5ac634e2ad98c841125a6f4c7
cirqueira-labs <jccirqueira@outlook.com>
2025-01-06 14:43:35 -0300

    fix:007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,48 +1,14 @@
-#!/bin/bash
+#see references
+#https://developer.bitcoin.org/reference/rpc/gettxout.html
+#https://bitcoin.stackexchange.com/questions/48504/how-to-check-if-an-output-has-been-spent
+block_hash=$(bitcoin-cli getblockhash 123321)
+utxo_array=$(bitcoin-cli getblock $block_hash 2 | jq -r '.tx[] |.txid as $txid |.vout[] |"\($txid),\(.n),\(.scriptPubKey.address)"')
 
-# Bloco alvo
-block_height=123321
-
-# Obter o hash do bloco
-block_hash=$(bitcoin-cli getblockhash $block_height)
-
-# Obter todas as transações do bloco
-txids=$(bitcoin-cli getblock $block_hash | jq -r '.tx[]')
-
-echo "Searching for unspent outputs in block $block_height..."
-
-# Lista para armazenar os UTXOs
-utxos=()
-
-# Iterar sobre as transações no bloco
-for txid in $txids; do
-    # Obter os detalhes da transação
-    raw_tx=$(bitcoin-cli getrawtransaction $txid true)
+while IFS=',' read -r txid vout address; do
+    gettxout_output=$(bitcoin-cli gettxout "$txid" "$vout")
     
-    # Iterar sobre os outputs da transação
-    outputs=$(echo "$raw_tx" | jq -c '.vout[]')
-
-    for output in $outputs; do
-        # Verificar se o output está na lista de UTXOs
-        is_spent=$(bitcoin-cli gettxout $txid $(echo "$output" | jq -r '.n'))
-        
-        if [[ -n "$is_spent" ]]; then
-            # Adicionar o UTXO à lista
-            utxos+=("$output")
-        fi
-    done
-done
-
-# Verificar se exatamente um UTXO foi encontrado
-if [[ ${#utxos[@]} -ne 1 ]]; then
-    echo "Expected exactly one unspent output, but found ${#utxos[@]}."
-    exit 1
-fi
-
-# Obter o endereço do único UTXO encontrado
-utxo=${utxos[0]}
-address=$(echo "$utxo" | jq -r '.scriptPubKey.addresses[0]')
-
-# Exibir o resultado
-echo "The single unspent output from block $block_height was sent to address: $address"
-
+    if [[ -n $gettxout_output ]]; then
+        echo $address
+        break
+    fi
+done <<< "$utxo_array"
